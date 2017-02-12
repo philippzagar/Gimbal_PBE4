1630a747dfdb7a491f1cd436b8acdc4d305be874
Philipp1999 <philipp.zagar@gmail.com>
2017-02-12 17:41:03 +0100

    Creating includes directory that contains classes, Delete unused files

---
+++ includes/functions.py
@@ -0,0 +1,69 @@
+# math library
+import math
+# I2C library
+import smbus
+
+class MPU:
+    # This is the address value read via the i2cdetect command
+    address = 0x68
+    # Power management registers
+    power_mgmt_1 = 0x6b
+    power_mgmt_2 = 0x6c
+
+    gyro_scale = 131.0
+    accel_scale = 16384.0
+
+    #TODO
+    gyro_scale_x = 87
+    gyro_scale_y = 4
+    gyro_scale_z = -27
+
+    accel_scale_x = -498
+    accel_scale_y = 1577
+    accel_scale_z = 1077
+
+    bus = smbus.SMBus(1)  # or bus = smbus.SMBus(1) for Revision 2 boards
+
+    # Now wake the 6050 up as it starts in sleep mode
+    bus.write_byte_data(address, power_mgmt_1, 0)
+
+    def read_all(self):
+        raw_gyro_data = self.bus.read_i2c_block_data(self.address, 0x43, 6)
+        raw_accel_data = self.bus.read_i2c_block_data(self.address, 0x3b, 6)
+
+        gyro_scaled_x = self.twos_compliment((raw_gyro_data[0] << 8) + raw_gyro_data[1]) / self.gyro_scale
+        gyro_scaled_y = self.twos_compliment((raw_gyro_data[2] << 8) + raw_gyro_data[3]) / self.gyro_scale
+        gyro_scaled_z = self.twos_compliment((raw_gyro_data[4] << 8) + raw_gyro_data[5]) / self.gyro_scale
+
+        accel_scaled_x = self.twos_compliment((raw_accel_data[0] << 8) + raw_accel_data[1]) / self.accel_scale
+        accel_scaled_y = self.twos_compliment((raw_accel_data[2] << 8) + raw_accel_data[3]) / self.accel_scale
+        accel_scaled_z = self.twos_compliment((raw_accel_data[4] << 8) + raw_accel_data[5]) / self.accel_scale
+
+        return (gyro_scaled_x, gyro_scaled_y, gyro_scaled_z, accel_scaled_x, accel_scaled_y, accel_scaled_z)
+
+    @staticmethod
+    def twos_compliment(val):
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    @staticmethod
+    def dist(a, b):
+        return math.sqrt((a * a) + (b * b))
+
+    def get_x_rotation(self, x,y,z):
+        radians = math.atan2(y, self.dist(x,z))
+        return math.degrees(radians)
+
+    def get_y_rotation(self, x,y,z):
+        radians = math.atan2(x, self.dist(y,z))
+        return -math.degrees(radians)
+
+    """
+    Not possible due to drift of Yaw value
+
+    def get_z_rotation(self, x,y,z):
+        radians = math.atan2()
+        return math.degrees(radians)
+    """
